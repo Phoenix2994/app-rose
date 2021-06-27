0ebd80756f64a9509fbb09727e2c96ca41c35708
Phoenix2994 <filippobianco2@gmail.com>
2021-06-27 16:32:13 +0200

    te-refactoring-1

--- src/app/pages/tool-ex/select-teams/select-teams.module.ts
+++ src/app/pages/tool-ex/select-teams/select-teams.module.ts
@@ -4,17 +4,38 @@ import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTabsModule } from '@angular/material/tabs';
+
 import { SelectTeamsPageRoutingModule } from './select-teams-routing.module';
 
 import { SelectTeamsPage } from './select-teams.page';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+const MAT_MODULES = [
+  MatTableModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatInputModule,
+  MatCheckboxModule,
+  MatTabsModule,
+];
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    SelectTeamsPageRoutingModule
+    SharedModule,
+    SelectTeamsPageRoutingModule,
+    ...MAT_MODULES,
   ],
-  declarations: [SelectTeamsPage]
+  declarations: [SelectTeamsPage],
 })
 export class SelectTeamsPageModule {}
--- src/app/pages/tool-ex/select-teams/select-teams.page.ts
+++ src/app/pages/tool-ex/select-teams/select-teams.page.ts
@@ -1,5 +1,7 @@
+import { SelectionModel } from '@angular/cdk/collections';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { IPlayer } from 'src/app/shared/model/player';
 import { Team } from 'src/app/shared/model/team';
 import { DataLoaderService } from 'src/app/shared/services/data-loader.service';
 
@@ -12,18 +14,60 @@ export class SelectTeamsPage implements OnInit {
   firstTeamId: number;
   secondTeamId: number;
   teams: Team[];
-
+  initialSelection = [];
+  allowMultiSelect = true;
+  selection = new SelectionModel<IPlayer>(
+    this.allowMultiSelect,
+    this.initialSelection
+  );
+  displayedColumns: string[] = ['role', 'name', 'select'];
+  ownPlayers: IPlayer[];
+  otherPlayers: IPlayer[];
   constructor(private dataLoader: DataLoaderService, private router: Router) {}
 
   ngOnInit() {
     this.teams = this.dataLoader.getTeams();
   }
 
+  computeRoleColor(role: string) {
+    switch (role) {
+      case 'P':
+        return '#F8AB29';
+      case 'D':
+        return '#63C623';
+      case 'C':
+        return '#2E6BE6';
+      case 'A':
+        return '#F21A3C';
+    }
+  }
+
   changeFirstTeam(event) {
     this.firstTeamId = event.detail.value;
+    this.ownPlayers = [...this.dataLoader.getTeam(this.firstTeamId).players]
+      .concat(
+        this.dataLoader.getTeam(this.firstTeamId).borrowed,
+        this.dataLoader.getTeam(this.firstTeamId).youth
+      )
+      .filter(
+        (player) =>
+          player.contractType === 'TITOLO DEFINITIVO' ||
+          player.contractType === 'PRESTITO (OBBLIGO)'
+      );
   }
+
   changeSecondTeam(event) {
     this.secondTeamId = event.detail.value;
+    this.otherPlayers = [...this.dataLoader.getTeam(this.secondTeamId).players]
+      .concat(
+        this.dataLoader.getTeam(this.secondTeamId).borrowed,
+        this.dataLoader.getTeam(this.secondTeamId).youth
+      )
+      .filter(
+        (player) =>
+          player.contractType === 'TITOLO DEFINITIVO' ||
+          player.contractType === 'PRESTITO (OBBLIGO)'
+      );
   }
 
   navigateToTool() {
@@ -33,5 +77,9 @@ export class SelectTeamsPage implements OnInit {
         secondTeamId: this.secondTeamId,
       },
     });
+  }
+
+  toggleSelect(player) {
+    this.selection.toggle(player);
   }
 }
