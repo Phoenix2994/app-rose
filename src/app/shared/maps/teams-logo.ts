0bfea1dbacdb113eb8a7421a5f5fbe3784acf500
Phoenix2994 <filippobianco2@gmail.com>
2021-08-01 19:19:54 +0200

    0.1.16

--- src/app/shared/maps/teams-logo.ts
+++ src/app/shared/maps/teams-logo.ts
@@ -1,6 +1,6 @@
 export const LOGO_MAP = {
   0: 'presence.png',
-  1: 'lei_ce_sta.jpeg',
+  1: 'lei_ce_sta.png',
   2: 'jonica.png',
   3: 'vitanews.png',
   4: 'phoenix.png',
@@ -9,6 +9,6 @@ export const LOGO_MAP = {
   7: 'npg.png',
   8: 'aka.png',
   9: 'loco.png',
-  10: 'rude.jpeg',
+  10: 'dynamo.jpeg',
   11: 'das.png',
 };
